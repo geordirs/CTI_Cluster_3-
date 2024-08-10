a2c045eaac461a53de2952f979c9ed912a2a0fc9
geordirs <geordirs@gmail.com>
2024-08-10 23:44:10 +1000

    database changes

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -28,43 +28,3 @@ def init_db():
     logger.info("Creating database tables...")
     Base.metadata.create_all(bind=engine, checkfirst=True)
     logger.info("Database tables created successfully.")
-
-
-
-''' TEST
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"  # Usa una base de datos en memoria para las pruebas
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def init_db():
-    logger.info("Importing models...")
-    from . import models  # Importa todos los modelos
-    logger.info("Models imported successfully.")
-    logger.info("Creating database tables...")
-    Base.metadata.create_all(bind=engine)
-    logger.info("Database tables created successfully.")
-    
-    # Verificar las tablas creadas
-    from sqlalchemy import inspect
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    logger.info(f"Tables in the database: {tables}")
-    
-    for table in tables:
-        columns = [column['name'] for column in inspector.get_columns(table)]
-        logger.info(f"Columns in {table}: {columns}")
-
-'''
